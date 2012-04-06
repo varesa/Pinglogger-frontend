297b3bfb9112c0e97d7c6df1d7618324c2844e76
Esa Varemo <esa@kuivanto.fi>
2012-04-06 20:08:55 +0300

    Something hafl-done about decreasing the amount of values

--- graph.php
+++ graph.php
@@ -15,12 +15,28 @@ ksort($pings);				// Make sure that the values are
 					// chronological order
 
 
-if($_GET['mode'] == 0) {		// Fixed mode
+if($_GET['mode'] == 1) {		// Fixed mode
     $graph = new Graph($_GET['sizex'],$_GET['sizey']);
-} elseif($_GET['mode'] == 1) {		// Scaled mode
+} elseif($_GET['mode'] == 2) {		// Scaled mode
     $graph = new Graph(count($pings)/50*$_GET['factor'],$_GET['sizey']);
 } else {
     $graph = new Graph(800,600);
+}
+
+$xsize = $graph->img->plotwidth;
+$count = count($pings);
+print($xsize . ", " . $count);
+
+echo "<br>";
+
+print(floor($count/$xsize) . " values per x-value + " . $count%$xsize);
+
+echo "<br>";
+
+for($x=0;$x<$xsize;$x++) {
+    $values = array_splice($pings, );
+    if(  array_values($pings)[ floor($count/$xsize) * $x ]  -  array_values($pings)[$x]  ==  )
+    print(max(array_values(array_slice($pings, $x*floor($count/$xsize)-1, floor($count/$xsize)))) . "<br>");
 }
 
 $graph->SetScale('datelin');
@@ -36,6 +52,6 @@ $plot->SetColor('black');
 
 $graph->Add($plot);
 
-$graph->Stroke();
+//$graph->Stroke();
 
-?> 
+?>
