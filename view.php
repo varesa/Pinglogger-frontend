c4ff0e260bd968a6d35754a97cca4fab4acaecf1
Esa Varemo <esa@kuivanto.fi>
2012-06-16 00:41:05 +0300

    Added checks for an empty parameter and invalid paramter (log name)

--- view.php
+++ view.php
@@ -20,6 +20,18 @@
 	###############
 	
 	$config = getConfig("viewer.conf");
+	
+	
+	if(isset($_GET['file']) && $_GET['file'] != "") {
+	    $file = rtrim($config['path_to_logs'],"/") . "/" . $_GET['file'];
+	} else {
+	    exit("Please use with the GET parameter 'file', which points to the logfile to be viewed");
+	}
+	
+	if(!is_file($file)) {
+	    exit("The logfile $file does not exist");
+	}
+
 	?>
 	
 	</body>
